bfac3dc39b4cecae7f4a9e662e81345d42d5d921
nehuyu <ne.huyu@gmail.com>
2016-08-27 03:22:26 +0900

    prepare for deploying on vps

---
+++ config/deploy.rb
@@ -0,0 +1,53 @@
+# config valid only for current version of Capistrano
+lock '3.6.1'
+
+
+
+
+set :application, '${APP_NAME}'
+set :repo_url, '${GIT_URL}'
+
+set :branch, 'master'
+set :deploy_to, '/var/www/app/${APP_NAME}'
+set :scm, :git
+set :log_level, :debug
+set :pty, true
+
+set :bundle_binstubs, nil
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets bundle public/system public/assets}
+set :default_env, { path: "/home/nehuyu/.rbenv/shims:/home/nehuyu/.rbenv/bin:$PATH" }
+set :keep_releases, 5
+
+namespace :deploy do
+  after :finishing, 'deploy:cleanup'
+end
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, '/var/www/my_app_name'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, 'config/database.yml', 'config/secrets.yml'
+
+# Default value for linked_dirs is []
+# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
---
+++ config/deploy/staging.rb
@@ -0,0 +1,74 @@
+# server-based syntax
+# ======================
+# Defines a single server with a list of roles and multiple properties.
+# You can define all roles on a single server, or split them:
+
+# server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
+# server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
+# server 'db.example.com', user: 'deploy', roles: %w{db}
+
+set :stage, :staging #環境名
+#各サーバの役割を記述
+role :app, %w{サーバ名} #アプリケーションサーバ
+role :web, %w{サーバ名} #webサーバ
+role :db,  %w{サーバ名} #DBサーバ
+#サーバ情報記述
+server 'サーバ名', #サーバ名
+user: 'ユーザ名', #実行ユーザ
+roles: %w{web app db}, # サーバの役割
+ssh_options: {
+    keys: %w(SSH接続用の秘密鍵ファイルパス),
+    auth_methods: %w(publickey),
+    password: 'パスワード'
+}
+
+# role-based syntax
+# ==================
+
+# Defines a role with one or multiple servers. The primary server in each
+# group is considered to be the first unless any  hosts have the primary
+# property set. Specify the username and a domain or IP for the server.
+# Don't use `:all`, it's a meta role.
+
+# role :app, %w{deploy@example.com}, my_property: :my_value
+# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
+# role :db,  %w{deploy@example.com}
+
+
+
+# Configuration
+# =============
+# You can set any configuration variable like in config/deploy.rb
+# These variables are then only loaded and set in this stage.
+# For available Capistrano configuration variables see the documentation page.
+# http://capistranorb.com/documentation/getting-started/configuration/
+# Feel free to add new variables to customise your setup.
+
+
+
+# Custom SSH Options
+# ==================
+# You may pass any option but keep in mind that net/ssh understands a
+# limited set of options, consult the Net::SSH documentation.
+# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
+#
+# Global options
+# --------------
+#  set :ssh_options, {
+#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+#    forward_agent: false,
+#    auth_methods: %w(password)
+#  }
+#
+# The server-based syntax can be used to override options:
+# ------------------------------------
+# server 'example.com',
+#   user: 'user_name',
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: 'user_name', # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: 'please use keys'
+#   }
